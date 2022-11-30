dce0025fb70bbef85753e358f29abca45a46a57c
sapanmozammel <sapanmozammel@gmail.com>
2022-11-30 21:10:41 +0600

    responsive

--- public/frontend/script/home.js
+++ public/frontend/script/home.js
@@ -22,3 +22,22 @@ document.addEventListener("DOMContentLoaded", function () {
     });
     splide2.mount();
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+    const button = document.getElementById("mlm-toggler-button");
+    const menu = document.getElementById("mlm-mobile-menu");
+    var mobile_menu_toggled = false;
+    button.addEventListener(
+        "click",
+        function () {
+            if (mobile_menu_toggled == false) {
+                mobile_menu_toggled = true;
+                menu.classList.add("active-menu");
+            } else {
+                mobile_menu_toggled = false;
+                menu.classList.remove("active-menu");
+            }
+        },
+        false
+    );
+});
