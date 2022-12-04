b9f013a79747d77cc586b66a138a2e36f6b806b9
safiul-dev <parsonal494@gmail.com>
2022-12-04 12:26:22 +0600

    some changes

--- resources/js/Page/User/BinaryTree.jsx
+++ resources/js/Page/User/BinaryTree.jsx
@@ -1,13 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { binaryTreeData } from '../../hooks/queries/user/binaryTreeData';
-
 const BinaryTree = () => {
 
     const { data:treeDatas, isLoading, refetch } = binaryTreeData();
 
-
     const binaryData = (datas) => {
-        
+        var arr = []
+
+       if (datas?.length > 0) {
+        var users = Object.entries(datas)
+        for (let i=0; i<users.length; i++) {
+            // if (!users[i]['sponsor_id']){
+            //     arr[users[i]['id']] = users[i]
+            // }
+            return arr;
+        }
+       }
+
+        console.log(arr)
     }
   return (
     <>
@@ -15,15 +25,7 @@ const BinaryTree = () => {
                 <div className="flex flex-1 flex-col lg:pl-64">
                     <main className="flex-1 py-8">
                         <div className="container">
-dsdfsdfdsf
-                                {
-                                    treeDatas?.map((user) => (
-                                        <div key={user.id}>
-                                        hello world
-                                        </div>
-                                    ))
-                                }
-                        </div>
+
                     </main>
                 </div>
             </div>
