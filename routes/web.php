2386e48796e647d43a66a575c45ac1a4c0d5d688
safiul-dev <parsonal494@gmail.com>
2022-12-05 20:46:04 +0600

    route setted

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ use App\Http\Controllers\Staff\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 // dashboard page route
-Route::get('/{any}', [DashboardController::class, 'index'])->where('any', '.*');
+Route::get('/staff/{slug?}', [DashboardController::class, 'index']);
 // end dashboard page route
 
 Route::get('/', [HomeCotroller::class, 'index'])->name('home')->name('index.home.page');
