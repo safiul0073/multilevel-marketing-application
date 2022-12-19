9e5e5e5e3954c15fccf70b9ea2bafa4a2a2e2a7f
safiul-dev <parsonal494@gmail.com>
2022-12-19 18:20:29 +0600

    some changes

---
+++ resources/views/frontend/contents/dashboard/index.blade.php
@@ -0,0 +1,22 @@
+@extends('frontend.layouts.app')
+@push('custom_style')
+{{-- here some custome style --}}
+@endpush
+@section('custome_style')
+{{-- here some style --}}
+
+@endsection
+@section('content')
+    <div>
+        this is user dashboard
+    </div>
+@endsection
+
+@push('custom_scipt')
+{{-- some sort js you can write here --}}
+
+@endpush
+
+@section('custome_scipt')
+
+@endsection
