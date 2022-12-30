234ce2f8186918abee6a88508892a177be0c8e9c
safiul-dev <parsonal494@gmail.com>
2022-12-30 21:08:07 +0600

    backend pagination fix

--- resources/js/Page/User/Index.jsx
+++ resources/js/Page/User/Index.jsx
@@ -74,7 +74,7 @@ const Index = () => {
                                         <LoaderAnimation />
                                     ) : (
                                         <>
-                                        {users?.length ?
+                                        {users?.data?.length ?
                                         <div className="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
                                             <div className="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
                                                 <div className="overflow-hidden shadow ring-1 ring-black ring-opacity-5 md:rounded-lg">
