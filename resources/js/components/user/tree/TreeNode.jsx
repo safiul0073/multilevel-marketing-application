b22fdfa5f6e227eedbe6e93c51c0f7f539d4c4ab
safiul-dev <parsonal494@gmail.com>
2022-12-31 16:24:21 +0600

    user tree to create user navigate fix:

--- resources/js/components/user/tree/TreeNode.jsx
+++ resources/js/components/user/tree/TreeNode.jsx
@@ -4,7 +4,7 @@ import { useEffect, useState } from "react";
 import { NewAddView } from "./NewAddView";
 import UserView from "./UserView";
 
-export default function TreeNode({ node, before = false, sponsor = null }) {
+export default function TreeNode({ node, before = false, sponsor = null, position = '' }) {
     const [firstChild, setFirstChild] = useState();
     const [secondChild, setSecondChild] = useState();
 
@@ -22,11 +22,11 @@ export default function TreeNode({ node, before = false, sponsor = null }) {
         <div className="flex flex-col justify-start items-center basis-1/2 shrink-0 min-w-max w-full">
             <UserView user={node} before={before} />
             <div className="w-full grid grid-cols-2 shrink-0 relative after:absolute after:bottom-full after:left-1/2 after:-translate-x-1/2 after:w-1 after:h-2.5 after:bg-gray-400 before:absolute before:h-1 before:top-0 before:left-[25%] before:right-[25%] before:bg-gray-400">
-                <TreeNode node={firstChild} before={true} sponsor={node?.id} />
-                <TreeNode node={secondChild} before={true} sponsor={node?.id} />
+                <TreeNode node={firstChild} before={true} position="left" sponsor={node?.id} />
+                <TreeNode node={secondChild} before={true} position="right" sponsor={node?.id} />
             </div>
         </div>
     ) : (
-        <NewAddView referral={sponsor} />
+        <NewAddView position={position} referral={sponsor} />
     );
 }
