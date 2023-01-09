5c709b5f87f9ed4b2507778163d4ee07c00a3379
safiul-dev <parsonal494@gmail.com>
2023-01-09 22:50:49 +0600

    route serialized

--- routes/staff.php
+++ routes/staff.php
@@ -60,9 +60,9 @@ Route::middleware('auth:staff')->group(function () {
 
 
     // user route list
-    Route::prefix('user')->name('info.user.')->group(function () {
+    Route::prefix('user')->name('user.')->group(function () {
         Route::resource('/', UserController::class);
-        Route::post('info/update', [UserController::class, 'update'])->name('update');
+        Route::post('info/update', [UserController::class, 'update'])->name('info.update');
         Route::get('referral-list/{user}', [UserHelperController::class, 'userReferrals']);
         // user Helper
         Route::get('binary', [UserHelperController::class, 'userBinaryTreeData']);
