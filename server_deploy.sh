c0e20fd272a7428e0142801e4364289ad64a99dd
safiul-dev <parsonal494@gmail.com>
2023-01-10 23:49:07 +0600

    git test action

--- server_deploy.sh
+++ server_deploy.sh
@@ -6,7 +6,8 @@ echo "Deploying application ..."
 # Enter maintenance mode
 (php artisan down --message 'The app is being (quickly!) updated. Please try again in a minute.') || true
     # Update codebase
-    git pull origin production
+    git fetch origin deploy
+    git reset --hard origin/deploy
 
     # Install dependencies based on lock file
     composer install --no-interaction --prefer-dist --optimize-autoloader
