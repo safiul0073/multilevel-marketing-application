a3e9649c1784fd7ea33a77993e226cf01e9dc9aa
safiul-dev <parsonal494@gmail.com>
2023-01-11 15:37:30 +0600

    script change

--- routes/web.php
+++ routes/web.php
@@ -66,6 +66,7 @@ Route::get('check-sponsor/', [RegisterController::class, 'checkSponsor'])->name(
 Route::get('check-user/', [RegisterController::class, 'checkUser'])->name('check.user');
 Route::post('save-user', [RegisterController::class, 'saveUser'])->name('save.user');
 
+
 // not found route
 Route::get('not-found', [NotFoundController::class, 'index'])->name('not.found');
 
