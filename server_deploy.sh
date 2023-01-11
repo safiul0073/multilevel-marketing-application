27335854f486592b7c957c87b51693d51bfa5719
safiul-dev <parsonal494@gmail.com>
2023-01-11 16:09:17 +0600

    script change and try hard

--- server_deploy.sh
+++ server_deploy.sh
@@ -11,7 +11,7 @@ echo "Deployment started ..."
 git pull origin production
 
 # Install composer dependencies
-composer install --no-dev --no-interaction --prefer-dist --optimize-autoloader
+composer install --ignore-platform-reqs
 
 # Clear the old cache
 php artisan clear-compiled
