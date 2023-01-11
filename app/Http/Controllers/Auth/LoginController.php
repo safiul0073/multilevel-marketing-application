6f457f8457885b7c7dd83fcb0ef98a7682fae2bc
safiul-dev <parsonal494@gmail.com>
2023-01-11 23:38:09 +0600

    login log comment out

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -73,7 +73,7 @@ class LoginController extends Controller
      */
     protected function authenticated(Request $request, $user)
     {
-        LoginActivity::addToLog();
+        // LoginActivity::addToLog();
     }
     /**
      * Create a new controller instance.
