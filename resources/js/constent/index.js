a1e25af580e37086ea8240c14f603e59504eb65a
safiul-dev <parsonal494@gmail.com>
2023-01-15 00:09:37 +0600

    http to https used

--- resources/js/constent/index.js
+++ resources/js/constent/index.js
@@ -1,4 +1,4 @@
 
-export const APIURL = import.meta.env.VITE_PUBLIC_API_URL ?? "http://aqebbd.com/api";
+export const APIURL = import.meta.env.VITE_PUBLIC_API_URL ?? "https://aqebbd.com/api";
 
-export const LIVE_URL = import.meta.env.VITE_PUBLIC_LIVE_URL ?? 'http://aqebbd.com/';
+export const LIVE_URL = import.meta.env.VITE_PUBLIC_LIVE_URL ?? 'https://aqebbd.com/';
