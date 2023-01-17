c76b422a56b18023ba580424d3e4d4f9d518e760
safiul-dev <parsonal494@gmail.com>
2023-01-17 22:57:48 +0600

    incentive bonus worked

---
+++ resources/js/hooks/queries/bonus/index.js
@@ -0,0 +1,21 @@
+import { userAxios } from "../../../config/axios.config";
+import { APIURL } from "../../../constent";
+
+export const incentiveSearch = async (inputData) => {
+    const res = await userAxios.post(
+      `${APIURL}/staff/incentive/search`,
+      inputData
+    );
+
+    return res?.data?.data?.string_data ?? res?.data?.data?.json_object;
+  };
+
+  export const incentiveBonusGive = async (inputData) => {
+    const res = await userAxios.post(
+      `${APIURL}/staff/incentive/bonus-give`,
+      inputData
+    );
+
+    return res?.data?.data?.string_data ?? res?.data?.data?.json_object;
+  };
+
