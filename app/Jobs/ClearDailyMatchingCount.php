bea765563afaddcb61422bff592792934b70f3c5
safiul-dev <parsonal494@gmail.com>
2023-01-23 10:59:29 +0600

    scheduler added with dynamic

--- app/Jobs/ClearDailyMatchingCount.php
+++ app/Jobs/ClearDailyMatchingCount.php
@@ -15,7 +15,6 @@ class ClearDailyMatchingCount implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
-    public $matching_settings;
     /**
      * Create a new job instance.
      *
@@ -23,7 +22,7 @@ class ClearDailyMatchingCount implements ShouldQueue
      */
     public function __construct()
     {
-        $this->matching_settings = config('mlm.bonus.matching');
+
     }
 
     /**
@@ -33,12 +32,12 @@ class ClearDailyMatchingCount implements ShouldQueue
      */
     public function handle()
     {
-
+        $matching_settings = config('mlm.bonus.matching');
         $users = User::select('id', 'left_count', 'right_count')->get();
-        if ($this->matching_settings['pair_type'] == 'Auto') {
-            $users->map(function ($user) {
-                $lf_count = $user->left_count > $this->matching_settings['pair_count'] ? $user->left_count - $this->matching_settings['pair_count'] : $user->left_count;
-                $rt_count = $user->right_count > $this->matching_settings['pair_count'] ? $user->right_count - $this->matching_settings['pair_count'] : $user->right_count;
+        if ($matching_settings['pair_type'] == 'Auto') {
+            $users->map(function ($user) use($matching_settings) {
+                $lf_count = $user->left_count > $matching_settings['pair_value'] ? $user->left_count - $matching_settings['pair_value'] : $user->left_count;
+                $rt_count = $user->right_count > $matching_settings['pair_value'] ? $user->right_count - $matching_settings['pair_value'] : $user->right_count;
                 if ($lf_count > $rt_count) {
                     $user->right_count = 0;
                     $user->left_count = $lf_count;
@@ -51,12 +50,34 @@ class ClearDailyMatchingCount implements ShouldQueue
                 }
                 $user->save();
             });
-        } else if ($this->matching_settings['pair_type'] == 'Flash:Carry') {
-            $users->map(function ($user) {
-                $lf_count = $user->left_count > $this->matching_settings['pair_count'] ? $user->left_count - $this->matching_settings['pair_count'] : $user->left_count;
-                $rt_count = $user->right_count > $this->matching_settings['pair_count'] ? $user->right_count - $this->matching_settings['pair_count'] : $user->right_count;
-                $user->right_count = $rt_count;
+        } else if ($matching_settings['pair_type'] == 'Flash:Carry') {
+            $users->map(function ($user) use($matching_settings) {
+                // $lf_count = $user->left_count > $matching_settings['pair_value'] ? $user->left_count - $matching_settings['pair_value'] : $user->left_count;
+                $rt_count = $user->right_count > $matching_settings['pair_value'] ? $user->right_count - $matching_settings['pair_value'] : $user->right_count;
                 $user->left_count = 0;
+                $user->right_count = $rt_count;
+                $user->save();
+            });
+        } else if ($matching_settings['pair_type'] == 'Carry:Flash') {
+            $users->map(function ($user) use($matching_settings) {
+                $lf_count = $user->left_count > $matching_settings['pair_value'] ? $user->left_count - $matching_settings['pair_value'] : $user->left_count;
+                // $rt_count = $user->right_count > $matching_settings['pair_value'] ? $user->right_count - $matching_settings['pair_value'] : $user->right_count;
+                $user->left_count = $lf_count;
+                $user->right_count = 0;
+                $user->save();
+            });
+        } else if ($matching_settings['pair_type'] == 'Carry:Carry') {
+            $users->map(function ($user) use($matching_settings) {
+                $lf_count = $user->left_count > $matching_settings['pair_value'] ? $user->left_count - $matching_settings['pair_value'] : $user->left_count;
+                $rt_count = $user->right_count > $matching_settings['pair_value'] ? $user->right_count - $matching_settings['pair_value'] : $user->right_count;
+                $user->left_count = $lf_count;
+                $user->right_count = $rt_count;
+                $user->save();
+            });
+        } else if ($matching_settings['pair_type'] == 'Flash:Flash') {
+            $users->map(function ($user) use($matching_settings) {
+                $user->left_count = 0;
+                $user->right_count = 0;
                 $user->save();
             });
         }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use Carbon\Carbon;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -17,6 +19,10 @@ class DatabaseSeeder extends Seeder
         // \App\Models\User::factory(10)->create();
 
         \App\Models\User::factory()->create();
+        \App\Models\TaskScheduler::create([
+            'title' => "matching",
+            'date_time' => geMatchingSchedulerDateTime()
+        ]);
         // $cats = \App\Models\Category::factory(10)->create();
 
         // foreach ($cats as $cat) {
