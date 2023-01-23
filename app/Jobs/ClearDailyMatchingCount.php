bd19693b6a5b2216c49636c2cbe8f4be918f8e6a
safiul-dev <parsonal494@gmail.com>
2023-01-23 11:11:22 +0600

    some updated

--- app/Jobs/ClearDailyMatchingCount.php
+++ app/Jobs/ClearDailyMatchingCount.php
@@ -33,49 +33,60 @@ class ClearDailyMatchingCount implements ShouldQueue
     public function handle()
     {
         $matching_settings = config('mlm.bonus.matching');
+        $matching_count = $matching_settings['pair_value'];
         $users = User::select('id', 'left_count', 'right_count')->get();
         if ($matching_settings['pair_type'] == 'Auto') {
-            $users->map(function ($user) use($matching_settings) {
-                $lf_count = $user->left_count > $matching_settings['pair_value'] ? $user->left_count - $matching_settings['pair_value'] : $user->left_count;
-                $rt_count = $user->right_count > $matching_settings['pair_value'] ? $user->right_count - $matching_settings['pair_value'] : $user->right_count;
-                if ($lf_count > $rt_count) {
-                    $user->right_count = 0;
-                    $user->left_count = $lf_count;
-                } else if ($lf_count < $rt_count) {
-                    $user->left_count = 0;
-                    $user->right_count = $rt_count;
-                }else {
-                    $user->left_count = $lf_count;
-                    $user->right_count = $rt_count;
+            $users->map(function ($user) use($matching_count) {
+                if ($user->left_count > $matching_count && $user->right_count > $matching_count) {
+
+                    $lf_count = $user->left_count - $matching_count;
+                    $rt_count = $user->right_count - $matching_count;
+                    if ($lf_count > $rt_count) {
+                        $user->right_count = 0;
+                        $user->left_count = $lf_count;
+                    } else if ($lf_count < $rt_count) {
+                        $user->left_count = 0;
+                        $user->right_count = $rt_count;
+                    }else {
+                        $user->left_count = $lf_count;
+                        $user->right_count = $rt_count;
+                    }
+                    $user->save();
                 }
-                $user->save();
+
             });
         } else if ($matching_settings['pair_type'] == 'Flash:Carry') {
-            $users->map(function ($user) use($matching_settings) {
-                // $lf_count = $user->left_count > $matching_settings['pair_value'] ? $user->left_count - $matching_settings['pair_value'] : $user->left_count;
-                $rt_count = $user->right_count > $matching_settings['pair_value'] ? $user->right_count - $matching_settings['pair_value'] : $user->right_count;
-                $user->left_count = 0;
-                $user->right_count = $rt_count;
-                $user->save();
+            $users->map(function ($user) use($matching_count) {
+                if ($user->left_count > $matching_count && $user->right_count > $matching_count) {
+                    // $lf_count = $user->left_count > $matching_count ? $user->left_count - $matching_count : $user->left_count;
+                    $rt_count = $user->right_count - $matching_count;
+                    $user->left_count = 0;
+                    $user->right_count = $rt_count;
+                    $user->save();
+                }
             });
         } else if ($matching_settings['pair_type'] == 'Carry:Flash') {
-            $users->map(function ($user) use($matching_settings) {
-                $lf_count = $user->left_count > $matching_settings['pair_value'] ? $user->left_count - $matching_settings['pair_value'] : $user->left_count;
-                // $rt_count = $user->right_count > $matching_settings['pair_value'] ? $user->right_count - $matching_settings['pair_value'] : $user->right_count;
-                $user->left_count = $lf_count;
-                $user->right_count = 0;
-                $user->save();
+            $users->map(function ($user) use($matching_count) {
+                if ($user->left_count > $matching_count && $user->right_count > $matching_count) {
+                    $lf_count = $user->left_count - $matching_count;
+                    // $rt_count = $user->right_count > $matching_count ? $user->right_count - $matching_count : $user->right_count;
+                    $user->left_count = $lf_count;
+                    $user->right_count = 0;
+                    $user->save();
+                }
             });
         } else if ($matching_settings['pair_type'] == 'Carry:Carry') {
-            $users->map(function ($user) use($matching_settings) {
-                $lf_count = $user->left_count > $matching_settings['pair_value'] ? $user->left_count - $matching_settings['pair_value'] : $user->left_count;
-                $rt_count = $user->right_count > $matching_settings['pair_value'] ? $user->right_count - $matching_settings['pair_value'] : $user->right_count;
-                $user->left_count = $lf_count;
-                $user->right_count = $rt_count;
-                $user->save();
+            $users->map(function ($user) use($matching_count) {
+                if ($user->left_count > $matching_count && $user->right_count > $matching_count) {
+                    $lf_count =  $user->left_count - $matching_count;
+                    $rt_count =  $user->right_count - $matching_count;
+                    $user->left_count = $lf_count;
+                    $user->right_count = $rt_count;
+                    $user->save();
+                }
             });
         } else if ($matching_settings['pair_type'] == 'Flash:Flash') {
-            $users->map(function ($user) use($matching_settings) {
+            $users->map(function ($user) {
                 $user->left_count = 0;
                 $user->right_count = 0;
                 $user->save();
