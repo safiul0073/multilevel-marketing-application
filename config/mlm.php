09efc250296b24a191112bb2fd1b7c449518d205
safiul-dev <parsonal494@gmail.com>
2023-02-04 23:05:29 +0600

    change report api added also office setting added into config and api

--- config/mlm.php
+++ config/mlm.php
@@ -3,15 +3,21 @@
 return [
     "bonus" => [
         "matching" => [
-            "pair_types" => ["Auto", "Flash:Carry", "Carry:Flash", "Carry:Carry", "Flash:Flash"],
-            "pair_type" => "Auto",
-            "pair_value" => 31,
+            "pair_types"    => ["Auto", "Flash:Carry", "Carry:Flash", "Carry:Carry", "Flash:Flash"],
+            "pair_type"     => "Auto",
+            "pair_value"    => 31,
             "pair_amount"   => 100,
-            "in_day"   => 1,
-            "end_time" => date('H:i', strtotime( "11:59 PM" ))
+            "in_day"        => 1,
+            "end_time"      => date('H:i', strtotime( "11:59 PM" ))
         ],
-        "gen"      => [10,9,8,7,6,2,2,2,2,2],
-        "incentive"=> 100
+        "gen"       => [10,9,8,7,6,2,2,2,2,2],
+        "incentive" => 100
     ],
-    "balance_transfer" => 50
+    "balance_transfer" => 50,
+    "office" => [
+        "location"  => "@142 Poran Masto Road 902100 Bangde Britatu Sans Francisco",
+        "phone"     => "+01234567890",
+        "email"     => "example@gmail.com"
+    ],
+    "logo" => "/image/logo.png"
 ];
--- routes/staff.php
+++ routes/staff.php
@@ -10,7 +10,7 @@ use App\Http\Controllers\Staff\V1\EpinHelperController;
 use App\Http\Controllers\Staff\V1\InceptiveBonusController;
 use App\Http\Controllers\Staff\V1\LoginInfo;
 use App\Http\Controllers\Staff\V1\MediaController;
-use App\Http\Controllers\Staff\V1\OptionController;
+use App\Http\Controllers\Staff\V1\Settings\OptionController;
 use App\Http\Controllers\Staff\V1\PackageController;
 use App\Http\Controllers\Staff\V1\PackageHelperController;
 use App\Http\Controllers\Staff\V1\PaymentMethodController;
@@ -106,12 +106,14 @@ Route::middleware('auth:staff')->group(function () {
         Route::get('to-earned', [App\Http\Controllers\Staff\V1\Reports\UserController::class, 'topEarned']);
         Route::get('to-sponsor', [App\Http\Controllers\Staff\V1\Reports\UserController::class, 'topSponsor']);
         Route::get('package-purchase', [App\Http\Controllers\Staff\V1\Reports\UserController::class, 'packagePurchaseList']);
+        Route::get('charge', [App\Http\Controllers\Staff\V1\Reports\ChargeController::class]);
     });
 
     // settings
     Route::prefix('settings/')->group(function () {
         Route::get('bonus', [OptionController::class, 'getBonus']);
         Route::get('transfer-charge', [OptionController::class, 'getTransferCharge']);
+        Route::get('office-settings', [OptionController::class, 'getOfficeSettings']);
         Route::post('bonus',[OptionController::class, 'storeOption']);
     });
 });
