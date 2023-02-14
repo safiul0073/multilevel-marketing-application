45272f977cc0882d538515a49681dafce1253160
safiul-dev <parsonal494@gmail.com>
2023-02-14 13:03:57 +0600

    remove product name

--- app/Http/Controllers/Staff/V1/EpinController.php
+++ app/Http/Controllers/Staff/V1/EpinController.php
@@ -55,7 +55,6 @@ class EpinController extends Controller
                 'type'          => $request->type,
                 'quantity'      => $request->quantity,
                 'product_id'    => $product->id,
-                'product_name'  => $product->name,
                 'customer_name' => $request->customer_name,
                 'customer_phone'=> $request->customer_phone,
             ]);
@@ -115,7 +114,6 @@ class EpinController extends Controller
                     'cost'          => $request->cost,
                     'type'          => $request->type,
                     'product_id'    => $request->product_id,
-                    'product_name'  => $product->name,
                     'customer_name' => $request->customer_name,
                     'customer_phone'=> $request->customer_phone,
                 ]);
