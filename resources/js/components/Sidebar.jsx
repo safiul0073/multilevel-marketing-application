65ffaf7a1972b99ff358a80b4785f1102e2b7cef
safiul-dev <parsonal494@gmail.com>
2023-02-21 20:57:37 +0600

    setting topbar button removed

--- resources/js/components/Sidebar.jsx
+++ resources/js/components/Sidebar.jsx
@@ -32,6 +32,7 @@ function classNames(...classes) {
 
 const Sidebar = () => {
     const navigate = useNavigate();
+    const { user } = UseStore()
     const activePatch = window.location.pathname
     const { removeAuth, removeUser } = UseStore();
     const [sidebarOpen, setSidebarOpen] = useState(false);
@@ -460,14 +461,14 @@ const Sidebar = () => {
                                 <Menu.Button className="flex max-w-xs items-center rounded-full bg-white text-sm focus:outline-none focus:ring-2 focus:ring-cyan-500 focus:ring-offset-2 lg:rounded-md lg:p-2 lg:hover:bg-gray-50">
                                     <img
                                         className="h-8 w-8 rounded-full"
-                                        src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                                        src={user?.image ? user?.image?.url : "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"}
                                         alt=""
                                     />
                                     <span className="ml-3 hidden text-sm font-medium text-gray-700 lg:block">
                                         <span className="sr-only">
                                             Open user menu for{" "}
                                         </span>
-                                        Emilia Birch
+                                        {user?.first_name + ' ' + (user?.last_name ? user?.last_name :'')}
                                     </span>
                                     <ChevronDownIcon
                                         className="ml-1 hidden h-5 w-5 flex-shrink-0 text-gray-400 lg:block"
@@ -485,7 +486,7 @@ const Sidebar = () => {
                                 leaveTo="transform opacity-0 scale-95"
                             >
                                 <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                                    <Menu.Item>
+                                    {/* <Menu.Item>
                                         {({ active }) => (
                                             <a
                                                 href="#"
@@ -510,14 +511,14 @@ const Sidebar = () => {
                                                 Settings
                                             </a>
                                         )}
-                                    </Menu.Item>
+                                    </Menu.Item> */}
                                     <Menu.Item>
                                         {({ active }) => (
                                             <button
                                                 onClick={logoutOption}
                                                 className={classNames(
                                                     active ? "bg-gray-100" : "",
-                                                    "block px-4 py-2 text-sm text-gray-700"
+                                                    "block px-4 py-2 text-sm text-gray-700 w-full"
                                                 )}
                                             >
                                                 Logout
