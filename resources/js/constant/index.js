b4f0e817e17506ebcbb199309a8009569e160969
safiul-dev <parsonal494@gmail.com>
2023-03-07 00:35:51 +0600

    some changes made

--- resources/js/constant/index.js
+++ resources/js/constant/index.js
@@ -1,5 +1,5 @@
 
-export const APIURL = import.meta.env.VITE_PUBLIC_API_URL ?? "https://aqebbd.com/api";
+export const APIURL = import.meta.env.VITE_PUBLIC_API_URL ?? "https://staging.aqebbd.com/api";
 
 export const LIVE_URL = import.meta.env.VITE_PUBLIC_LIVE_URL ?? 'https://staging.aqebbd.com/';
 
