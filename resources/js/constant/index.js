13f5a06ab7a0771e09cc076a0246ab01a9be44f0
safiul-dev <parsonal494@gmail.com>
2023-03-07 00:20:12 +0600

    some change

--- resources/js/constant/index.js
+++ resources/js/constant/index.js
@@ -1,6 +1,6 @@
 
 export const APIURL = import.meta.env.VITE_PUBLIC_API_URL ?? "https://staging.aqebbd.com/api";
 
-export const LIVE_URL = import.meta.env.VITE_PUBLIC_LIVE_URL ?? 'https://aqebbd.com/';
+export const LIVE_URL = import.meta.env.VITE_PUBLIC_LIVE_URL ?? 'https://staging.aqebbd.com/';
 
 export const TOKEN_NAME = "ZXTXY-ambed-temp";
