fc954b0a8e3580394271f0b1f1de4c9fa46b9688
safiul-dev <parsonal494@gmail.com>
2023-03-16 21:34:08 +0600

    some changes

--- resources/js/components/Layouts/Auth.jsx
+++ resources/js/components/Layouts/Auth.jsx
@@ -1,13 +1,12 @@
+import React from "react";
+import { useRedirectRoute } from "../../hooks/others";
 
-import React, { useEffect } from 'react'
-import { useRedirectRoute } from '../../hooks/others';
-
-export default function AuthLayout ({children}) {
+export default function AuthLayout({ children }) {
     useRedirectRoute();
-  return (
-    <div>
-        {/* only login component using this layout */}
-        {children}
-    </div>
-  )
+    return (
+        <div>
+            {/* only login component using this layout */}
+            {children}
+        </div>
+    );
 }
