4fa5ae91908c3145af09d1c7c59a16640f7352de
safiul-dev <parsonal494@gmail.com>
2023-03-31 17:10:56 +0600

    incentive checking

--- app/Http/Controllers/Frontend/IncentiveBonusController.php
+++ app/Http/Controllers/Frontend/IncentiveBonusController.php
@@ -34,8 +34,7 @@ class IncentiveBonusController extends Controller
                                    ->where('bonus_type', Bonuse::INCENTIVE)
                                    ->where('status', false)
                                    ->sum('amount');
-        dd($incentive_bonus,(float) $request->amount);
-
+        
         if ($incentive_bonus != (float) $request->amount){
             return redirect()->back()->with(['error' => 'Bonus not match']);
         }
