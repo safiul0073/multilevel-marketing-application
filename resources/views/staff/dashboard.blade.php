7fdd6ee32f047dd3d9ebaa5506a40956868feec7
safiul-dev <parsonal494@gmail.com>
2023-04-20 15:48:12 +0600

    some changes

--- resources/views/staff/dashboard.blade.php
+++ resources/views/staff/dashboard.blade.php
@@ -5,7 +5,6 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>{{ config('app.name') }}</title>
-
     <!-- React render initial Scripts -->
     @viteReactRefresh
     @vite(['resources/css/app.css','resources/js/app.js'])
