0ac7f7f3fbc466c2fa2a5372b338d5c47f3f0380
safiul-dev <parsonal494@gmail.com>
2023-05-08 21:56:10 +0600

    some changes teliscop added

--- resources/js/constant/index.js
+++ resources/js/constant/index.js
@@ -1,8 +1,8 @@
 export const APIURL =
-    import.meta.env.VITE_PUBLIC_API_URL ?? "https://staging.aqebbd.com/api";
+    import.meta.env.VITE_PUBLIC_API_URL ?? "http://www.ekhandani.com/api";
 
 export const LIVE_URL =
-    import.meta.env.VITE_PUBLIC_LIVE_URL ?? "https://staging.aqebbd.com/";
+    import.meta.env.VITE_PUBLIC_LIVE_URL ?? "http://www.ekhandani.com/";
 
 export const API_FULL_URL = `${APIURL}/v1/staff`;
 
